454200f942bdac0fc33b697f003230908ca9d70f
NakamaShp <khlfaalhsn5@gmail.com>
2026-01-05 21:52:34 +0700

    refactor(routes): move landing page to public route group

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,20 @@
+import { Footer } from "@/components/layouts/footer";
+import { FloatingHeader } from "@/components/layouts/navbar/floating-header.tsx";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body>
+        {/* Pasang Header Floating di sini */}
+        <FloatingHeader />
+
+        {children}
+      </body>
+      <Footer />
+    </html>
+  );
+}
